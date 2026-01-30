ba1b4bf2334dc8afd5803592c86666e7b535fa13
tejas kahirnar <tejaskhairnar256@gmail.com>
2026-01-30 15:26:20 +0530

    save auth flow

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { register, sendOtp, verifyOtp } from "../controllers/auth.controller.js";
+import { getAllUsers, getUserById, register, sendOtp, verifyOtp } from "../controllers/auth.controller.js";
+
 
 const router = express.Router();
+router.get("/users",getAllUsers)
+router.get("/users/:id",getUserById);
 router.post("/register", register);
 router.post("/send-otp", sendOtp);
 router.post("/verify-otp", verifyOtp);
